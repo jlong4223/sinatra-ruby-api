7c71d5cb39145b80f11b87a60c12acd4fe7598b4
Jared Long <jlong4223@gmail.com>
2021-06-06 12:11:53 -0500

    uses faker data to test another route

--- server.rb
+++ server.rb
@@ -12,3 +12,14 @@ end
 get "/route/:param" do
     {"The param you provided": params["param"], query: params["query"]}.to_json
 end
+
+get "/minecraft/biomes" do
+    {
+        "biome": Faker::Games::Minecraft.biome, 
+        "biome2": Faker::Games::Minecraft.biome,
+        "biome3": Faker::Games::Minecraft.biome,
+        "biome4": Faker::Games::Minecraft.biome,
+        "biome5": Faker::Games::Minecraft.biome,
+        "biome6": Faker::Games::Minecraft.biome,
+    }.to_json
+end
